55bea7fd30471a1071ff27283ec7859f971efe0d
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2023-11-07 23:43:51 -0500

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import { Vista } from "./components/Vista";
 import { Vista1 } from "./components/Vista1";
 import { Vista3 } from "./components/Vista3";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Vista />}>
           **<Route index element={<Vista1 />}></Route>
           <Route path="/vista" element={<Vista3 />}></Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
